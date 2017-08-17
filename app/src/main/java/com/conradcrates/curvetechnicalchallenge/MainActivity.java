be91a0e2c2abfb430473d34cf9cdd2f41f1e3901
Conrad Crates <conrad-crates@hotmail.co.uk>
2017-08-17 12:15:20 +0100

    Defining the 6 ediText views to be used

--- app/src/main/java/com/conradcrates/curvetechnicalchallenge/MainActivity.java
+++ app/src/main/java/com/conradcrates/curvetechnicalchallenge/MainActivity.java
@@ -2,6 +2,9 @@ package com.conradcrates.curvetechnicalchallenge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.widget.EditText;
+import android.widget.GridLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridLayout layout = (GridLayout)findViewById(R.id.layout_grid);
+        for(int i = 0; i < 6; i++){
+            EditText et = new EditText(this);
+            et.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+            GridLayout.LayoutParams params =new GridLayout.LayoutParams();
+            params.height = GridLayout.LayoutParams.WRAP_CONTENT;
+            params.width = 500;
+            et.setLayoutParams(params);
+            layout.addView(et);
+        }
     }
 }
